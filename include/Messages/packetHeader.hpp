f9ad65d57347daaf4565efbce949116020e1a4ed
Gr3gbug <l.digregoriozitel@studenti.unipi.it>
2021-04-19 18:14:08 +0200

    update packetHeader

--- include/Messages/packetHeader.hpp
+++ include/Messages/packetHeader.hpp
@@ -7,11 +7,28 @@
 #include <string>
 #include <vector>
 
-struct header
+struct Header
 {
     /* data size = 9 Bytes */
     short unsigned int  type{0}; // TODO check allpacket type
     unsigned int        counter{0};
     unsigned int        payload_size{0};
+
+    void serialize(Header *packet, char *ser_buf)
+    {
+        short unsigned int* sui = (short unsigned int*)ser_buf;
+        *sui = packet->type;    sui++;
+        unsigned int* ui = (unsigned int*)sui;
+        *ui = packet->counter;  ui++;
+        *ui = packet->payload_size; ui++;
+    }
+    void deserilize(char *ser_buf, Header *packet)
+    {
+        short unsigned int *usi = (short unsigned int*)ser_buf;
+        packet->type = *usi;  usi++;
+        unsigned int* ui = (unsigned int*)usi;
+        packet->counter = *ui; ui++;
+        packet->payload_size = *ui; ui++;
+    }
 };
 #endif
