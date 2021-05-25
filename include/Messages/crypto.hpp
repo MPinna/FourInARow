227e5a54d70ee9172b65ce99c5946ed716d9223a
Gr3gbug <l.digregoriozitel@studenti.unipi.it>
2021-05-25 17:34:42 +0200

    member functions for crypto.cpp and crypto.hpp

--- include/Messages/crypto.hpp
+++ include/Messages/crypto.hpp
@@ -20,21 +20,24 @@ typedef struct __attribute__((packed))
     unsigned short int  _sig_size{0};   
     unsigned char *     _signature{NULL};   
     
-    // DESCRIPTION: member structure
+    // DESCRIPTION: Setter
     void setDigest(unsigned char *digest);
     void setSignature(unsigned char *sig);
+    // DESCRIPTION: member structure
     void serialize(unsigned char * to_ser_buf);
     void deserialize(unsigned char * ser_buf);
 } Auth;
 
 typedef struct __attribute__((packed))
-{
+{   // TOCHECK is all that we need ?
     unsigned short int  _tag_size{0};
     unsigned char *     _tag{NULL};
-     
+    
+    // DESCRIPTION: Setter
+    void setTag(unsigned char * tag); 
     // DESCRIPTION: member structure
-    void serialize(char * to_ser_buf);
-    void deserialize(char * ser_buf);
+    void serialize(unsigned char * to_ser_buf);
+    void deserialize(unsigned char * ser_buf);
 } AEAD; // Auth Encryption Auth Data
 
 typedef struct __attribute__((packed))
@@ -42,19 +45,23 @@ typedef struct __attribute__((packed))
     unsigned short int  _lenght{0};
     unsigned char *     _cert{NULL};
 
+    // DESCRIPTION: Setter
+    void setCert(unsigned char * cert);
     // DESCRIPTION: member structure
-    void serialize(char * to_ser_buf);
-    void deserialize(char * ser_buf);
+    void serialize(unsigned char * to_ser_buf);
+    void deserialize(unsigned char * ser_buf);
 } Certificate;
 
 typedef struct __attribute__((packed))
 {
     unsigned int        _nonce{0}; // NOTE: Value computed as (g)^(a) mod p
 	unsigned short int  _dh_lenght{0};
-	unsigned char *     _dh_key[];
+	unsigned char *     _dh_key;
 
+    // DESCRIPTION: Setter
+    void setDHKey(unsigned char * dhkey);
     // DESCRIPTION: member structure
-    void serialize(char * to_ser_buf);
-    void deserialize(char * ser_buf);
+    void serialize(unsigned char * to_ser_buf);
+    void deserialize(unsigned char * ser_buf);
 } DHKey;
 #endif
--- src/Messages/crypto.cpp
+++ src/Messages/crypto.cpp
@@ -53,13 +53,107 @@ Auth::deserialize(unsigned char *ser_buf)
     memcpy(&dig_size, ser_buf + pos, sizeof(uint16_t));
     this->_dig_size = ntohs(dig_size);
     pos += sizeof(uint16_t);
-    this->_digest = (unsigned char *)malloc(this->_dig_size);
-    memcpy(this->_digest, ser_buf + pos, this->_dig_size);
+    this->setDigest(ser_buf + pos);
     pos += this->_dig_size;
     memcpy(&sig_size, ser_buf + pos, sizeof(uint16_t));
     this->_sig_size = ntohs(sig_size);
     pos += sizeof(uint16_t);
-    this->_signature = (unsigned char *)malloc(this->_sig_size);
-    memcpy(this->_signature, ser_buf + pos, this->_sig_size);
-    pos += this->_sig_size;
+    this->setSignature(ser_buf + pos);
+}
+
+void
+AEAD::setTag(unsigned char *tag)
+{
+    this->_tag = (unsigned char *)malloc(this->_tag_size);
+    memcpy(this->_tag, tag, this->_tag_size);
+}
+
+void
+AEAD::serialize(unsigned char *to_ser)
+{
+    short int pos{0};
+    uint16_t tag_size{htons(this->_tag_size)};
+
+    memcpy(to_ser, &tag_size, sizeof(uint16_t));
+    pos += sizeof(uint16_t);
+    memcpy(to_ser + pos, this->_tag, this->_tag_size);
+}
+
+void
+AEAD::deserialize(unsigned char *ser_buf)
+{
+    short int pos{0};
+    uint16_t tag_size;
+
+    memcpy(&tag_size, ser_buf, sizeof(uint16_t));
+    this->_tag_size = ntohs(tag_size);
+    pos += sizeof(uint16_t);
+    this->setTag(ser_buf + pos);
+}
+
+void
+Certificate::setCert(unsigned char * cert)
+{
+    this->_cert = (unsigned char *)malloc(this->_lenght);
+    memcpy(this->_cert, cert, this->_lenght);
+}
+
+void
+Certificate::serialize(unsigned char * to_ser)
+{
+    short int pos{0};
+    uint16_t lenght{htons(this->_lenght)};
+
+    memcpy(to_ser, &lenght, sizeof(uint16_t));
+    pos += sizeof(uint16_t);
+    memcpy(to_ser + pos, this->_cert, this->_lenght);
+}
+
+void
+Certificate::deserialize(unsigned char *ser_buf)
+{
+    short int pos{0};
+    uint16_t lenght{0};
+
+    memcpy(&lenght, ser_buf, sizeof(uint16_t));
+    this->_lenght = ntohs(lenght);
+    pos += sizeof(uint16_t);
+    this->setCert(ser_buf + pos);
+}
+
+void
+DHKey::setDHKey(unsigned char *dhkey)
+{
+    this->_dh_key = (unsigned char *)malloc(this->_dh_lenght);
+    memcpy(this->_dh_key, dhkey, this->_dh_lenght);
+}
+
+void
+DHKey::serialize(unsigned char *to_ser)
+{
+    unsigned short int pos{0};
+    uint16_t dh_lenght{htons(this->_dh_lenght)};
+    uint32_t nonce{htonl(this->_nonce)};
+
+    memcpy(to_ser, &nonce, sizeof(uint32_t));
+    pos += sizeof(uint32_t);
+    memcpy(to_ser + pos, &dh_lenght, sizeof(uint16_t));
+    pos += sizeof(uint16_t);
+    memcpy(to_ser + pos, this->_dh_key, this->_dh_lenght);
+}
+
+void
+DHKey::deserialize(unsigned char *ser_buf)
+{
+    unsigned short int pos{0};
+    uint16_t dh_lenght{0};
+    uint32_t nonce{0};
+
+    memcpy(&nonce, ser_buf, sizeof(uint32_t));
+    this->_nonce = ntohl(nonce);
+    pos += sizeof(uint32_t);
+    memcpy(&dh_lenght, ser_buf + pos, sizeof(uint16_t));
+    this->_dh_lenght = ntohs(dh_lenght);
+    pos += sizeof(uint16_t);
+    this->setDHKey(ser_buf + pos);
 }
