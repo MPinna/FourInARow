f33f85b44bfd59311d2b54a4de04992f1d3e6f69
Gr3gbug <l.digregoriozitel@studenti.unipi.it>
2021-07-15 18:52:50 +0200

    x509 openssl functions implementation

--- libs/OpensslX509/include/x509.hpp
+++ libs/OpensslX509/include/x509.hpp
@@ -1,3 +1,17 @@
 #ifndef _OPENSSL_X509_H_
 #define _OPENSSL_X509_H_
+#include <openssl/x509.h>
+#include <string>
+
+int retrieve_cert(std::string file_name, X509** cacert);
+
+int retrieve_crl(std::string file_name, X509** cacert);
+
+int compare_subject_name(X509* cert, std::string str_name);
+
+int setup_store(X509_STORE* store, std::string ca_cert_file_name, std::string crl_file_name);
+
+// TODO: check if needed or not
+int send_cert();
+
 #endif
